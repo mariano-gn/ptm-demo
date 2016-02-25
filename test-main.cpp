82bb261c679421bf685bd183d625dfc5f69e7fcc
Mariano Gonzalez <mg0701tt@gmail.com>
2016-02-25 01:24:26 -0300

    Add source code for the demo.

--- test-main.cpp
+++ test-main.cpp
@@ -22,7 +22,55 @@ SOFTWARE.
 
 #include <iostream>
 #include <string>
+#include <vector>
+
+class A {
+public:
+    A(int val1, int val2) : data_member1(val1), data_member2(val2) {}
+    
+    void function_member() {
+        std::cout << "Hello from A::function_member!" <<
+            "\n\tMy data_member1 is: " << data_member1 <<
+            "\n\tAnd my data_member2 is: " << data_member2 << std::endl;
+    }
+
+    int data_member1 = 0;
+    int data_member2 = 1;
+};
+
+template<typename T, typename U>
+void execute_on_object(T&& object, U&& method) {
+    // Important: due to operators precedence,
+    // you need to resolve first either "object.*method" or "object->*method" before actually calling the method.
+    (object.*method)();
+}
+
+template<typename T, typename U>
+void print_from_object(T&& object, U&& member) {
+    std::cout << "Printing value from object: " << object.*member << std::endl;
+}
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    // Initialize a vector of A's.
+    std::vector<A> vec{ {1, 2}, {15, 3}, {5, 30}, {100, 10000} };
+
+    // Grab the pointer to the function member and the 2 data members.
+    auto pointer_to_fn_member = &A::function_member;
+    auto pointer_to_fst_data_member = &A::data_member1;
+    auto pointer_to_snd_data_member = &A::data_member2;
+
+    std::cout << "Iterating to execute.\n";
+    for (auto item : vec) {
+        execute_on_object(item, pointer_to_fn_member);
+    }
+
+    std::cout << "Iterating to print the first value of each member.\n";
+    for (auto item : vec) {
+        print_from_object(item, pointer_to_fst_data_member);
+    }
+
+    std::cout << "Iterating to print the second value of each member.\n";
+    for (auto item : vec) {
+        print_from_object(item, pointer_to_snd_data_member);
+    }
 }
